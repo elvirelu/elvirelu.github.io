3b2bb3f21e3fb54b1a582cf87a70f1cb7ec9a229
elvire <elvirelu@gmail.com>
2022-11-26 13:31:34 -0500

    adjust navbar

--- js/contenuAccueil.js
+++ js/contenuAccueil.js
@@ -1,9 +1,13 @@
 let contenuAccueil = () =>{
     $('body').css('background', 'white');
 let contenu = `
-<div class="row justify-content-between">
+<table>
+<tr><br><br></tr>
+<tr>
+<td>
 <div class="col-5"><img src="../images/intro/salon.jpg"></div>
-<div class="col-5">
+</td>
+<td>
 <div class="para">
 <h2>Historique de l’entreprise </h2>
 Notre Atelier de coiffure située à Montréal.
@@ -12,12 +16,10 @@ C’est une nouvelle entreprise œuvrant dans le domaine de la coiffure.
 Atelier de coiffure est un salon professionnel, dynamique et jeune qui emploie maintenant 5 personnes. 
 Nous désirons offrir le meilleur service pour ses clients et ses employés avec l’ajout d’un système de gestion informatique.
 </div>
-</div>
-</div>
-<br><br>
-<div class="row justify-content-between">
-
-<div class="col-5">
+</td>
+</tr>
+<tr>
+<td>
     <div class="para">
         <h2>Une liste de ses principales réalisations</h2>   
       Le salon de coiffure fonctionne toujours très bien. Les coiffeurs en fonction dans le salon sont talentueux, et ont beaucoup d’expériences dans le domaine de coiffure. 
@@ -25,24 +27,24 @@ Nous désirons offrir le meilleur service pour ses clients et ses employés avec
       Autre service proposé qui semble bien accueilli par les clients est la vente de produits de soins capillaires.
   
   </div>
-    </div>
+</td>
+<td>
 <div class="col-5"><img src="../images/intro/man.jpg"></div>
-</div>
-<br><br>
-<div class="row"></div>
-
-<div class="row justify-content-between">
+</td>
+</tr>
+<tr>
+<td>
 <div class="col-5"><img src="../images/intro/produits.jpg"></div>
-<div class="col-5"><div class="para">
+</td>
+<td>
+<div class="para">
     <h2>Les objectifs du service ou du produit offert </h2>
     Atelier de coiffure est un salon qui offrent des services typiques de coiffure : toutes sortes de coupes de cheveux, shampoing, brushing, coloration et mèche, extensions capillaires, etc.
 Le salon vend aussi une assez grande gamme de produits de soins capillaires : produits coiffants pour cheveux colorés, pour cheveux fins, etc.
 </div>
-</div>
-
-</div>
-
-</div>
+</td>
+</tr>
+</table>
 `;
 $('#contenuIndex').html(contenu);
 }
